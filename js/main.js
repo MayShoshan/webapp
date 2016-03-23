28dcfbcfd551a884371592771036d6777e5c9e5e
May Shoshan <may20078@gmail.com>
2016-03-23 04:03:41 +0200

    cont3.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
+function _on_load() {
+    updateinput();
+    updateselect();
+    updateiframe();
+    updateinput2();
+    updateselect2();
+    updateiframe2();
+    var ajaxnotifications = function (notif) {
 
+        var Data = notif;
+        var element = document.getElementById("notification-file");
+        element.innerHTML = Data.notification;
+        element.className = "notifications show";
+    };
+
+    var ajax_set = { method: 'GET', done: ajaxnotifications };
+    UTILS.ajax('./data/config.json', ajax_set)
+}
 /*****switch tabs*****/
 changetab();
 window.addEventListener("hashchange", changetab, false);
@@ -51,6 +68,7 @@ function changetab() {
         document.querySelector(".tabs>ul li:nth-child(4) a").className = "selected-tab";
     }
 }
+/*****URL check****/
 function isUrl(url) {
 
     var urlPatt = new RegExp("(http|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?");
@@ -59,10 +77,16 @@ function isUrl(url) {
     }
     return true;
 }
+/********cancel btn*********/
 function closesettings()
 {
     document.getElementById("settings-checkbox1").checked = false;
 }
+function closesettings2()
+{
+    document.getElementById("settings-checkbox2").checked = false;
+}
+/*******clear errors **********/
 function clear_errors()
 {
     document.getElementById("name1").nextElementSibling.hidden = true;
@@ -78,10 +102,112 @@ function clear_errors()
     document.getElementById("url3").nextElementSibling.hidden = true;
     document.getElementById("url3").className = "site-input";
 }
-function getsitesinfo()
+function clear_errors2()
 {
+    document.getElementById("name4").nextElementSibling.hidden = true;   
+    document.getElementById("name4").className = "site-input";
+    document.getElementById("name5").nextElementSibling.hidden = true;
+    document.getElementById("name5").className = "site-input";
+    document.getElementById("name6").nextElementSibling.hidden = true;
+    document.getElementById("name6").className = "site-input";
+    document.getElementById("url4").nextElementSibling.hidden = true;
+    document.getElementById("url4").className = "site-input";
+    document.getElementById("url5").nextElementSibling.hidden = true;
+    document.getElementById("url5").className = "site-input";
+    document.getElementById("url6").nextElementSibling.hidden = true;
+    document.getElementById("url6").className = "site-input";
+}
+
+
+
+
+
+function updateinput() {
+    if (localStorage.getItem('Data') != null) {
+        var jobject = JSON.parse(localStorage.getItem('Data'));
+        var Sites = jobject.Sites;
+        for (var i = 0; i < Sites.length; i++) {
+            document.querySelector('#name' + (i + 1)).value = Sites[i].Name;
+            document.querySelector('#url' + (i + 1)).value = Sites[i].Url;
+        }
+    }
+}
+function updateinput2() {
+    if (localStorage.getItem('Data') != null) {
+        var jobject = JSON.parse(localStorage.getItem('Data'));
+        var Sites2 = jobject.Sites2;
+        for (var i = 0; i < Sites2.length; i++) {
+            document.querySelector('#name' + (i + 4)).value = Sites2[i].Name;
+            document.querySelector('#url' + (i + 4)).value = Sites2[i].Url;
+        }
+    }
+}
+function updateiframe() {
+
+    if (document.querySelector("#quick-reports-tab .sites-sel").value != null && document.querySelector("#quick-reports-tab .sites-sel").value != "") {
+        document.querySelector("#quick-reports-tab .site-iframe").src = document.querySelector("#quick-reports-tab .sites-sel").value;
+        document.querySelector("#quick-reports-tab .site-iframe").hidden = false;
+        document.getElementById("QR-arrow").href = document.querySelector("#quick-reports-tab .sites-sel").value;
+    }
+    else
+        document.querySelector("#quick-reports-tab .site-iframe").hidden = true;
+}
+function updateiframe2() {
+
+    if (document.querySelector("#my-team-folders-tab .sites-sel").value != null && document.querySelector("#my-team-folders-tab .sites-sel").value != "") {
+        document.querySelector("#my-team-folders-tab .site-iframe").src = document.querySelector("#my-team-folders-tab .sites-sel").value;
+        document.querySelector("#my-team-folders-tab .site-iframe").hidden = false;
+        document.getElementById("QR-arrow2").href = document.querySelector("#my-team-folders-tab .sites-sel").value;
+    }
+    else
+        document.querySelector("#quick-reports-tab .site-iframe").hidden = true;
+}
+function updateselect() {
+
+
+    if (localStorage.getItem('Data') != null) {
+        var jobject = JSON.parse(localStorage.getItem('Data'));
+        var Sites = jobject.Sites;
+        var sites_sel = document.querySelector("#quick-reports-tab .sites-sel");
+
+        for (var i = sites_sel.length - 1  ; i >= 0 ; i--) {
+            sites_sel.remove(i);
+        }
+        for (var i = 0; i < Sites.length; i++) {
+            var option = document.createElement("option");
+            option.text = Sites[i].Name;
+            option.value = Sites[i].Url;
+            sites_sel.add(option);
+        }
+
+        updateiframe();
+    }
+}
+function updateselect2() {
+
     
-    var jobject = { Sites: [] };
+    if (localStorage.getItem('Data') != null) {
+        var jobject = JSON.parse(localStorage.getItem('Data'));
+        var Sites2 = jobject.Sites2;
+        var sites_sel = document.querySelector("#my-team-folders-tab .sites-sel");
+        for (var i = sites_sel.length - 1  ; i >= 0 ; i--) {
+            sites_sel.remove(i);
+        }
+        for (var i = 0; i < Sites2.length; i++) {
+            var option = document.createElement("option");
+            option.text = Sites2[i].Name;
+            option.value = Sites2[i].Url;
+            sites_sel.add(option);
+        }
+
+        updateiframe2();
+    }
+}
+/********site info*********/
+
+function getsitesinfo() {
+
+    var Sites = [];
     var name1 = document.getElementById("name1");
     var name2 = document.getElementById("name2");
     var name3 = document.getElementById("name3");
@@ -92,27 +218,24 @@ function getsitesinfo()
     var valid = true;
     /****checks for report1***/
     //name empty url not
-    if((name1.value!="")||(url1.value!=""))
-    {  
-        if ((name1.value == "") && (url1.value != ""))
-        {
-        name1.nextElementSibling.hidden = false;
-        name1.className = "site-input error-input";
-        valid = false;
-        } 
-        else {
-        //name valid and url invalid
-            if ((isUrl(url1.value) == false) && (name1.value != ""))
-            {
-            url1.nextElementSibling.hidden = false;
-            url1.className = "site-input error-input";
+    if ((name1.value != "") || (url1.value != "")) {
+        if ((name1.value == "") && (url1.value != "")) {
+            name1.nextElementSibling.hidden = false;
+            name1.className = "site-input error-input";
             valid = false;
+        }
+        else {
+            //name valid and url invalid
+            if ((isUrl(url1.value) == false) && (name1.value != "")) {
+                url1.nextElementSibling.hidden = false;
+                url1.className = "site-input error-input";
+                valid = false;
             }
         }
-        if(valid==true)
-            jobject.Sites.push({ "Name": name1.value, "Url": url1.value });
+        if (valid == true)
+            Sites.push({ "Name": name1.value, "Url": url1.value });
     }
-    if (((name2.value != "") || (url2.value != ""))&&(valid)) {
+    if (((name2.value != "") || (url2.value != "")) && (valid)) {
         if ((name2.value == "") && (url2.value != "")) {
             name2.nextElementSibling.hidden = false;
             name2.className = "site-input error-input";
@@ -127,9 +250,9 @@ function getsitesinfo()
             }
         }
         if (valid == true)
-            jobject.Sites.push({ "Name": name2.value, "Url": url2.value });
+            Sites.push({ "Name": name2.value, "Url": url2.value });
     }
-    if (((name3.value != "") || (url3.value != ""))&&(valid)) {
+    if (((name3.value != "") || (url3.value != "")) && (valid)) {
         if ((name3.value == "") && (url3.value != "")) {
             name3.nextElementSibling.hidden = false;
             name3.className = "site-input error-input";
@@ -144,16 +267,143 @@ function getsitesinfo()
             }
         }
         if (valid == true)
-            jobject.Sites.push({ "Name": name3.value, "Url": url3.value });
+            Sites.push({ "Name": name3.value, "Url": url3.value });
     }
     if (valid == true) {
-        
-        localStorage.setItem('Data', JSON.stringify(jobject));
+
+        if (localStorage.getItem('Data') != null) {
+            var jobject = JSON.parse(localStorage.getItem('Data'));
+            jobject.Sites = Sites;
+            localStorage.setItem('Data', JSON.stringify(jobject));
+        }
+        else {
+            var jobject = { Sites: Sites };
+            localStorage.setItem('Data', JSON.stringify(jobject));
+        }
+
+
+
         document.getElementById("settings-checkbox1").checked = false;
         updateinput();
         updateselect();
+
+    }
+
+
+}
+function getsitesinfo2() {
+    var Sites2 = [];
+    var name4 = document.getElementById("name4");
+    var name5 = document.getElementById("name5");
+    var name6 = document.getElementById("name6");
+    var url4 = document.getElementById("url4");
+    var url5 = document.getElementById("url5");
+    var url6 = document.getElementById("url6");
+    clear_errors2();
+    var valid = true;
+    /****checks for report1***/
+    //name empty url not
+    if ((name4.value != "") || (url4.value != "")) {
+        if ((name4.value == "") && (url4.value != "")) {
+            name4.nextElementSibling.hidden = false;
+            name4.className = "site-input error-input";
+            valid = false;
+        }
+        else {
+            //name valid and url invalid
+            if ((isUrl(url4.value) == false) && (name4.value != "")) {
+                url4.nextElementSibling.hidden = false;
+                url4.className = "site-input error-input";
+                valid = false;
+            }
+        }
+        if (valid == true)
+            Sites2.push({ "Name": name4.value, "Url": url4.value });
+    }
+    if (((name5.value != "") || (url5.value != "")) && (valid)) {
+        if ((name5.value == "") && (url5.value != "")) {
+            name5.nextElementSibling.hidden = false;
+            name5.className = "site-input error-input";
+            valid = false;
+        }
+        else {
+            //name valid and url invalid
+            if ((isUrl(url5.value) == false) && (name5.value != "")) {
+                url5.nextElementSibling.hidden = false;
+                url5.className = "site-input error-input";
+                valid = false;
+            }
+        }
+        if (valid == true)
+            Sites2.push({ "Name": name5.value, "Url": url5.value });
+    }
+    if (((name6.value != "") || (url6.value != "")) && (valid)) {
+        if ((name6.value == "") && (url6.value != "")) {
+            name6.nextElementSibling.hidden = false;
+            name6.className = "site-input error-input";
+            valid = false;
+        }
+        else {
+            //name valid and url invalid
+            if ((isUrl(url6.value) == false) && (name6.value != "")) {
+                url6.nextElementSibling.hidden = false;
+                url6.className = "site-input error-input";
+                valid = false;
+            }
+        }
+        if (valid == true)
+            Sites2.push({ "Name": name6.value, "Url": url6.value });
+    }
+    if (valid == true) {
+        if (localStorage.getItem('Data') != null) {
+            var jobject = JSON.parse(localStorage.getItem('Data'));
+            jobject.Sites2 = Sites2;
+            localStorage.setItem('Data', JSON.stringify(jobject));
+        }
+        else {
+            var jobject = { Sites2: Sites2 };
+            localStorage.setItem('Data', JSON.stringify(jobject));
+        }
+
+
+        document.getElementById("settings-checkbox2").checked = false;
+        updateinput2();
+        updateselect2();
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***********event listeners****************/
 document.getElementById("name1").addEventListener("input", function () {
     if (document.getElementById("name1").value != "") {
         document.getElementById("name1").className = "site-input";
@@ -172,7 +422,7 @@ document.getElementById("name3").addEventListener("input", function () {
         document.getElementById("name3").nextElementSibling.hidden = true;
     }
 }, false);
-document.getElementById("url1").addEventListener("input", function () {
+document.getElementById("url").addEventListener("input", function () {
     if (isUrl(document.getElementById("url1").value)) {
         document.getElementById("url1").className = "site-input";
         document.getElementById("url1").nextElementSibling.hidden = true;
@@ -193,70 +443,45 @@ document.getElementById("url3").addEventListener("input", function () {
     }
 
 }, false);
-
-function updateinput() {
-    if (localStorage.getItem('Data') != null) {
-        
-        var jobject = JSON.parse(localStorage.getItem('Data'));
-        var Sites = jobject.Sites;
-        for (var i = 0; i < Sites.length; i++)
-        {
-            document.querySelector('#name'+(i + 1)).value = Sites[i].Name;
-            document.querySelector('#url' + (i + 1)).value = Sites[i].Url;
-        }
+document.getElementById("name4").addEventListener("input", function () {
+    if (document.getElementById("name4").value != "") {
+        document.getElementById("name4").className = "site-input";
+        document.getElementById("name4").nextElementSibling.hidden = true;
     }
-}
-function _on_load()
-{
-    updateinput();
-    updateselect();
-}
-function updateiframe() {
-    alert("HEREEEEE");
-    if (document.querySelector("#quick-reports-tab .sites-sel").value != null && document.querySelector("#quick-reports-tab .sites-sel").value != "") {
-        alert("HEREEEEE222");
-        document.querySelector("#quick-reports-tab .site-iframe").src = document.querySelector("#quick-reports-tab .sites-sel").value;
-        alert("hereeee3");
-        document.querySelector("#quick-reports-tab .site-iframe").hidden = false;
-        document.getElementById("QR-arrow").href = document.querySelector("#quick-reports-tab .sites-sel").value;
+}, false);
+document.getElementById("name5").addEventListener("input", function () {
+    if (document.getElementById("name5").value != "") {
+        document.getElementById("name5").className = "site-input";
+        document.getElementById("name5").nextElementSibling.hidden = true;
     }
-    else
-        document.querySelector("#quick-reports-tab .site-iframe").hidden = true;
-}
-function updateselect() {
-    
-    alert("yayyy");
-    if (localStorage.getItem('Data') != null)
-    {
-        var jobject = JSON.parse(localStorage.getItem('Data'));
-        var Sites = jobject.Sites;
-        var sites_sel = document.querySelector("#quick-reports-tab .sites-sel");
-        alert(sites_sel.length);
-        for (var i = sites_sel.length-1  ; i >=0 ; i--)
-        {
-            sites_sel.remove(i);
-        }
-        alert(sites_sel.length);
-        alert(Sites.length);
-        for (var i = 0; i < Sites.length; i++) {
-            var option = document.createElement("option");
-            option.text = Sites[i].Name;
-            option.value = Sites[i].Url;
-            sites_sel.add(option);
-        }
-        
-        updateiframe();
+}, false);
+document.getElementById("name6").addEventListener("input", function () {
+    if (document.getElementById("name6").value != "") {
+        document.getElementById("name6").className = "site-input";
+        document.getElementById("name6").nextElementSibling.hidden = true;
     }
-}
+}, false);
+document.getElementById("url4").addEventListener("input", function () {
+    if (isUrl(document.getElementById("url4").value)) {
+        document.getElementById("url4").className = "site-input";
+        document.getElementById("url4").nextElementSibling.hidden = true;
+    }
+
+}, false);
+document.getElementById("url5").addEventListener("input", function () {
+    if (isUrl(document.getElementById("url5").value)) {
+        document.getElementById("url5").className = "site-input";
+        document.getElementById("url5").nextElementSibling.hidden = true;
+    }
+
+}, false);
+document.getElementById("url6").addEventListener("input", function () {
+    if (isUrl(document.getElementById("url6").value)) {
+        document.getElementById("url6").className = "site-input";
+        document.getElementById("url6").nextElementSibling.hidden = true;
+    }
+
+}, false);
 
 
 
-var ajaxnotifications = function (notif) {
-    var Data = notif;
-    var element=document.getElementById("notification-file");
-    element.innerHTML = Data.notification;
-    element.className = "notifications show";
-};
-
-var ajax_set = { method: 'GET', done: ajaxnotifications };
-UTILS.ajax('./data/config.json', ajax_set)
